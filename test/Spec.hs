f6dc39213e5627f08a8388c0c5dc296a144aeb81
Wojtek <wojt3k.chmielarz@gmail.com>
2017-01-27 10:57:11 +0100

    Tests update

--- test/Spec.hs
+++ test/Spec.hs
@@ -4,23 +4,23 @@ import Test.HUnit
 import MillerRabin
 import Keys
 
-foo :: Int -> (Int, Int)
-foo x = (1, x)
+testPrimalityWithPrimeNumber :: Test
+testPrimalityWithPrimeNumber = TestCase (assertEqual "for (isPrime 5)," True (isPrime 5))
 
-test1 :: Test
-test1 = TestCase (assertEqual "for (isPrime 5)," True (isPrime 5))
+testPrimalityWithCompositeNumber :: Test
+testPrimalityWithCompositeNumber = TestCase (assertEqual "for (isPrime 5)," False (isPrime 36))
 
-test2 :: Test
-test2 = TestCase (assertEqual "for (isPrime 5)," False (isPrime 36))
+testModulo :: Test
+testModulo  = TestCase (assertEqual "for (properMod 3 15)," 3 (properMod 3 15))
 
-test3 :: Test
-test3  = TestCase (assertEqual "for (properMod 3 15)," 3 (properMod 3 15))
-
-test4 :: Test
-test4  = TestCase (assertEqual "for (extendedEuclidean 2 5)," (-2,1) (extendedEuclidean 2 5))
+testExtendedEuclidean :: Test
+testExtendedEuclidean  = TestCase (assertEqual "for (extendedEuclidean 2 5)," (-2,1) (extendedEuclidean 2 5))
 
 main = do
   putStrLn "QuickCheck"
 
   putStrLn "HUnit"
-  runTestTT $ TestList [test1,test2,test3,test4]
+  runTestTT $ TestList [testPrimalityWithPrimeNumber,
+                        testPrimalityWithCompositeNumber,
+                        testModulo,
+                        testExtendedEuclidean]
